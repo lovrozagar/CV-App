32a83a2385b6f77739b95e5649dfbc9784196753
lovro <lovro.zagar5@gmail.com>
2023-03-28 07:07:29 -0900

    feat: add generate CV functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,38 @@
+import { forwardRef, useRef } from 'react'
+import ReactToPrint from 'react-to-print'
+import { Button } from '@mui/material'
 import Header from './components/Header'
 import './App.scss'
 import './styles/Paper.scss'
 
 function App() {
-  return <Paper />
+  function handleSave() {
+    const element = document.getElementById('paper')
+    window.print(element)
+  }
+
+  const componentRef = useComponentRef()
+
+  return (
+    <div>
+      <ReactToPrint
+        trigger={() => (
+          <Button variant='contained' className='print-button'>
+            GENERATE CV
+          </Button>
+        )}
+        content={() => componentRef.current}
+      />
+      <Paper ref={componentRef} />
+    </div>
+  )
 }
 
-function Paper() {
+const Paper = forwardRef((props, ref) => {
   return (
     <main
+      ref={ref}
+      id='paper'
       className='paper'
       style={{
         margin: '0 auto',
@@ -20,6 +44,12 @@ function Paper() {
       <Header />
     </main>
   )
+})
+
+function useComponentRef() {
+  const componentRef = useRef()
+
+  return componentRef
 }
 
 export default App
