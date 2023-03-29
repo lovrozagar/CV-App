e75f26078690c2c526559ba930be6fac24398d9f
lovro <lovro.zagar5@gmail.com>
2023-03-29 07:51:00 -0900

    refactor: change Links component to use it's on states instead of passing props from Header

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,26 +1,45 @@
-import { TextField, Fab, Button } from '@mui/material'
+// FUNCTIONALITY
+import React, { useEffect, useMemo, useState } from 'react'
+import uniqid from 'uniqid'
+// MUI
 import AddIcon from '@mui/icons-material/Add'
 import DeleteIcon from '@mui/icons-material/Delete'
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff'
-import CheckIcon from '@mui/icons-material/Check'
-import React, { useState } from 'react'
-import uniqid from 'uniqid'
+import { StyledButton } from './styled/StyledButtons'
+import { StyledInput } from './styled/styledInputs'
+import { StyledLink } from './styled/styledLinks'
+// ASSETS
+import chain from '../images/chain.svg'
 
 function Links() {
-  const defaultLinks = [
-    {
-      url: 'ninja@ninjamail.com',
-      placeholder: 'NinjaMail',
-      id: uniqid(),
-    },
-    {
-      url: 'https://github.com/lovrozagar',
-      placeholder: 'NinjaGitHub',
-      id: uniqid(),
-    },
-  ]
+  const defaultLinks = useMemo(
+    () => [
+      {
+        url: 'ninja@ninjamail.com',
+        placeholder: 'NinjaMail',
+        id: uniqid(),
+      },
+      {
+        url: 'https://github.com/lovrozagar',
+        placeholder: 'NinjaGitHub',
+        id: uniqid(),
+      },
+      {
+        url: 'https://www.linkedin.com/in/lovrozagar/',
+        placeholder: 'NinjaLinkedIn',
+        id: uniqid(),
+      },
+    ],
+    []
+  )
   const [links, setLinks] = useState(defaultLinks)
   const [areLinksOnEdit, setAreLinksOnEdit] = useState(false)
+
+  useEffect(() => {
+    if (links.length === 0) {
+      setLinks(defaultLinks)
+    }
+  }, [links, defaultLinks])
 
   function urlOrMail(input) {
     const regex = /^\S+@\S+\.\S+$/ // guess if link is mail
@@ -37,15 +56,16 @@ function Links() {
   }
 
   function toggleEditMode() {
-    setAreLinksOnEdit(!areLinksOnEdit)
     deleteEmptyLinks()
-    !links.length && setLinks(defaultLinks)
+    setAreLinksOnEdit(!areLinksOnEdit)
   }
 
   function deleteEmptyLinks() {
     setLinks((prevLinks) =>
       prevLinks.filter((link) => {
-        return link.url && link.placeholder && link.id
+        return (
+          link.url.trim() !== '' && link.placeholder.trim() !== '' && link.id
+        )
       })
     )
   }
@@ -76,76 +96,99 @@ function Links() {
     setLinks((links) => [...links, { url: '', placeholder: '', id: uniqid() }])
     console.log(links)
   }
-  return (
-    <div className='links'>
-      {areLinksOnEdit ? (
-        <div style={{ display: 'grid', gap: '1rem' }}>
-          <p>Slicing... Links</p>
-          {links.map((link) => (
-            <div
-              key={link.id}
-              style={{
-                display: 'grid',
-                gap: '0.5rem',
-                gridTemplateColumns: '1fr auto auto',
-              }}
-            >
-              <TextField
-                label='Full URL or Email'
-                defaultValue={link.url}
-                onChange={(e) => handleUrlChange(e, link.id)}
-              />
-              <TextField
-                label='Placeholder'
-                defaultValue={link.placeholder}
-                onChange={(e) => handlePlaceholderChange(e, link.id)}
-              />
-              <Button onClick={() => handleLinkDelete(link.id)}>
-                <DeleteIcon />
-              </Button>
-            </div>
-          ))}
+
+  function LinksInput() {
+    return (
+      <div style={{ display: 'grid', gap: '1rem' }}>
+        <p>Slicing... Links</p>
+        {links.map((link) => (
           <div
+            key={link.id}
             style={{
-              display: 'flex',
-              justifyContent: 'center',
+              display: 'grid',
+              gap: '0.5rem',
+              gridTemplateColumns: '1fr auto auto',
             }}
           >
-            <Button size='small' aria-label='add' onClick={handleLinkAdd}>
-              <AddIcon />
-            </Button>
-            <Button
-              size='small'
-              aria-label='toggle visibility'
-              onClick={toggleEditMode}
-            >
-              <VisibilityOffIcon />
-            </Button>
+            <StyledInput
+              label='Full URL or Email'
+              defaultValue={link.url}
+              onChange={(e) => handleUrlChange(e, link.id)}
+            />
+            <StyledInput
+              label='Placeholder'
+              defaultValue={link.placeholder}
+              onChange={(e) => handlePlaceholderChange(e, link.id)}
+            />
+            <StyledButton onClick={() => handleLinkDelete(link.id)}>
+              <DeleteIcon />
+            </StyledButton>
           </div>
-        </div>
-      ) : (
+        ))}
         <div
-          className='link-container'
           style={{
             display: 'flex',
             justifyContent: 'center',
-            gap: '1rem',
           }}
         >
-          {links.map((link) => {
-            return (
-              <a
-                href={urlOrMail(link.url)}
-                target='_blank'
-                rel='noreferrer'
-                onClick={handleClick}
-              >
-                {link.placeholder}
-              </a>
-            )
-          })}
+          <StyledButton size='small' aria-label='add' onClick={handleLinkAdd}>
+            <AddIcon />
+          </StyledButton>
+          <StyledButton
+            size='small'
+            aria-label='toggle visibility'
+            onClick={toggleEditMode}
+          >
+            <VisibilityOffIcon />
+          </StyledButton>
         </div>
-      )}
+      </div>
+    )
+  }
+
+  function LinksView() {
+    return (
+      <div
+        className='link-container'
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          gap: '1rem',
+        }}
+      >
+        {links.map((link) => {
+          return (
+            <StyledLink
+              className='link'
+              key={link.id}
+              href={urlOrMail(link.url)}
+              target='_blank'
+              rel='noreferrer'
+              onClick={handleClick}
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+              }}
+            >
+              <img
+                src={chain}
+                alt=''
+                style={{
+                  height: '50%%',
+                  paddingRight: '0.25rem',
+                }}
+              />
+              {link.placeholder}
+            </StyledLink>
+          )
+        })}
+      </div>
+    )
+  }
+
+  return (
+    <div className='links'>
+      {areLinksOnEdit ? <LinksInput /> : <LinksView />}
     </div>
   )
 }
